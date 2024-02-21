a2c8f58272220be6447da86adf96733efe15e52b
Suraj Raghu <surajraghu544@gmail.com>
2024-02-21 14:57:54 +0530

    fist project

---
+++ First/src/main/java/com/mail/First/App.java
@@ -0,0 +1,17 @@
+package com.mail.First;
+
+import com.mail.First.second.Second;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Second s= new Second();
+    	s.m1();
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ First/src/main/java/com/mail/First/second/Second.java
@@ -0,0 +1,8 @@
+package com.mail.First.second;
+
+public class Second {
+
+	public void m1() {
+		System.out.println("this is second method");
+	}
+}
